31f7dd6808616658f3899876e4882bf8e27d9252
kentaroy47 <kentaroy@stanford.edu>
2022-06-27 11:43:09 +0900

    add standard resnet

--- network/resnet.py
+++ network/resnet.py
@@ -1,31 +1,5 @@
 '''
-Properly implemented ResNet-s for CIFAR10 as described in paper [1].
-
-The implementation and structure of this file is hugely influenced by [2]
-which is implemented for ImageNet and doesn't have option A for identity.
-Moreover, most of the implementations on the web is copy-paste from
-torchvision's resnet and has wrong number of params.
-
-Proper ResNet-s for CIFAR10 (for fair comparision and etc.) has following
-number of layers and parameters:
-
-name      | layers | params
-ResNet20  |    20  | 0.27M
-ResNet32  |    32  | 0.46M
-ResNet44  |    44  | 0.66M
-ResNet56  |    56  | 0.85M
-ResNet110 |   110  |  1.7M
-ResNet1202|  1202  | 19.4m
-
-which this implementation indeed has.
-
-Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
-[2] https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
-
-If you use this implementation in you work, please don't forget to mention the
-author, Yerlan Idelbayev.
+PACT and uniform quantized resnets
 '''
 import torch
 import torch.nn as nn
@@ -33,15 +7,7 @@ import torch.nn.functional as F
 import torch.nn.init as init
 from module import ActFn, Conv2d, Linear
 from torch.autograd import Variable
-from pact_utils import QuantizedConv2d, PactReLU
-
-__all__ = ['ResNet', 'resnet20', 'resnet34']
-
-def _weights_init(m):
-    classname = m.__class__.__name__
-    #print(classname)
-    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
-        init.kaiming_normal_(m.weight)
+from pact_utils import QuantizedConv2d, PactReLU, QuantizedLinear
 
 class LambdaLayer(nn.Module):
     def __init__(self, lambd):
@@ -115,24 +81,34 @@ class BasicBlockUQ(nn.Module):
         return out
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10, abits=8, wbits=8, pact=False):
+    def __init__(self, block, num_blocks, num_classes=10, abits=8, wbits=8, pact=False, shallow=True):
+        """
+        shallow = True follows the resnet in pact and various works.
+        shallow = False is the standard resnet implementation.
+        """
         super(ResNet, self).__init__()
-        from pact_utils import QuantizedConv2d, QuantizedLinear
         self.abits = abits
         self.wbits = wbits
-        self.in_planes = 16
-        self.abits = abits
-
-        self.conv1 = QuantizedConv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False, abits=8, wbits=8)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.alpha1 = nn.Parameter(torch.tensor(10.))
-        self.ActFn = ActFn.apply
-        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1, expansion=1)
-        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2, expansion=1)
-        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2, expansion=1)
+        self.shallow = shallow
         self.relu = nn.ReLU()
-        self.linear = QuantizedLinear(64, num_classes, abits=abits, wbits=wbits)
-        self.apply(_weights_init)
+
+        if shallow:
+            self.in_planes = 16
+            self.conv1 = QuantizedConv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False, abits=8, wbits=8)
+            self.bn1 = nn.BatchNorm2d(16)
+            self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1, expansion=1)
+            self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2, expansion=1)
+            self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2, expansion=1)
+            self.linear = QuantizedLinear(64, num_classes, abits=abits, wbits=wbits)
+        else:
+            self.in_planes = 64
+            self.conv1 = QuantizedConv2d(3, self.in_planes, kernel_size=3, stride=1, padding=1, bias=False, abits=8, wbits=8)
+            self.bn1 = nn.BatchNorm2d(self.in_planes)
+            self.layer1 = self._make_layer(block, self.in_planes, num_blocks[0], stride=1, expansion=1)
+            self.layer2 = self._make_layer(block, self.in_planes*2, num_blocks[1], stride=2, expansion=1)
+            self.layer3 = self._make_layer(block, self.in_planes*4, num_blocks[2], stride=2, expansion=1)
+            self.layer3 = self._make_layer(block, self.in_planes*8, num_blocks[3], stride=2, expansion=1)
+            self.linear = QuantizedLinear(self.in_planes*8, num_classes, abits=abits, wbits=wbits)
 
     def _make_layer(self, block, planes, num_blocks, stride, expansion):
         strides = [stride] + [1]*(num_blocks-1)
@@ -148,18 +124,24 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
+        if not self.shallow:
+            out = self.layer4(out)
         out = F.avg_pool2d(out, out.size()[3])
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
 
-def resnet20(abits, wbits, pact=False):
+def resnet20(abits, wbits, pact=False, shallow=True):
     print("abit/wbit:", abits, wbits)
-    if not pact:
-        return ResNet(BasicBlockUQ, [3, 3, 3], abits=abits, wbits=wbits)
+    if shallow:
+        blocks = [3,3,3]
     else:
-        return ResNet(BasicBlockPACT, [3, 3, 3], abits=abits, wbits=wbits)
+        blocks = [2,2,2,2]
+    if not pact:
+        return ResNet(BasicBlockUQ, blocks, abits=abits, wbits=wbits, shallow=shallow)
+    else:
+        return ResNet(BasicBlockPACT, blocks, abits=abits, wbits=wbits, shallow=shallow)
 
 def test(net):
     import numpy as np
