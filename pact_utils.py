fafb4aa21c4004783adb1b18d3f4bc6a1adfbf4d
kentaroy47 <kentaroy@stanford.edu>
2023-10-23 11:12:06 +0900

    update

--- pact_utils.py
+++ pact_utils.py
@@ -321,7 +321,7 @@ class GaussianNoise(nn.Module):
             network to generate vectors with smaller values.
     """
 
-    def __init__(self, sigma=0.1, is_relative_detach=True, inference=False, white=False):
+    def __init__(self, sigma=0.1, is_relative_detach=True, inference=True, white=False):
         super().__init__()
         self.sigma = sigma
         self.is_relative_detach = is_relative_detach
